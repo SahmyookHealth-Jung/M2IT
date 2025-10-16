4a0dd46f37dcf89562007a1cd9c947ecc158883e
정승우 <downersw@gmail.com>
2025-10-16 16:44:29 +0900

    key event 공부

---
+++ Apps/Calculator2/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Calculator2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // 텍스트 박스2를 여러 줄, 스크롤 가능하게 설정하여 많은 이벤트 정보를 볼 수 있게 함
+            textBox2.Multiline = true;
+            textBox2.ScrollBars = ScrollBars.Both;
+
+            // setup events that listens on keypress
+            // textbox1에 이벤트 핸돌러(메서드) 연결
+            textBox1.KeyDown += TextBox1_KeyDown;
+            textBox1.KeyPress += TextBox1_KeyPress;
+            textBox1.KeyUp += TextBox1_KeyUp;
+
+        }
+
+
+        private void TextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            textBox2.AppendText($"KeyUp code: {e.KeyCode}, value: {e.KeyValue}, modifiers: {e.Modifiers}" + "\r\n");
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            textBox2.AppendText($"KeyDown code: {e.KeyCode}, value: {e.KeyValue}, modifiers: {e.Modifiers}" + "\r\n");
+            display.Text = textBox1.Text;
+        }
+        
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            textBox2.AppendText($"KeyPress keychar: {e.KeyChar}" + "\r\n");
+        }
+    }
+}
--- Memo/C# 계산기 응용.ver.md
+++ Memo/C# 계산기 응용.ver.md
@@ -1,3 +1,62 @@
 C# 계산기 응용.ver (숫자를 마우스 클릭 \&\& 키보드 입력 받기)
 
-* 
+
+
+\## KEY EVENT
+
+
+
+* KeyDown -물리적 키가 눌리는 순간 (e.KeyCode)
+* KeyPress - 눌린 키가 문자로 변환되어 입력될 때 (e.KeyChar)
+* KeyUp - 물리적인 키에서 손을 떼는 순간
+
+
+
+* e.KeyCode : 사용자가 누른 키의 이름 EX) A, ControlKey, F1 등)을 나타내는 열거형 값
+* e.KeyValue : KeyCode에 해당하는 정수 값
+* e.Modifiers : Shift, Ctrl, Alt 등의 조합 키가 함께 눌렸는지 여부를 나타냄
+
+
+
+\## 데이터(변수) 정의 -> 초기 환경 (생성자) 설정 -> 기능 구현(메서드)
+
+* 폼이 어떤 데이터를 관리해야 하는지를 생각하고 변수나 속성을 정의한다.
+
+=> 사고방식 : "이 계산기 폼이 작동하는 데 필요한 상태, 값, 설정은 무엇일까?"
+
+=> 위치 : public class Form1 : Form 안
+
+EX) private string currentInput = "0"; // 현재 입력중인 숫자
+
+EX) private double lastValue = 0; // 직전에 계산된 값
+
+EX) private string currentOperator = ""; // 현재 선택된 연산자
+
+
+
+* 폼이 처음 화면에 나타날 때 준비해야 할 초기 환경을 생성자 안에 설정
+
+=> 사고방식 : "폼이 켜지자마자 UI를 준비하고, 이벤트 처리 장치를 연결해야 하는데?"
+
+=> 위치 : public Form1() 안
+
+EX) textBox1.KeyDown += TextBox1\_KeyDown; (메서드 연결) / 이벤트와 기능 연결
+
+
+
+구조적 사고 흐름 
+
+★ 무엇을 관리할까
+
+
+
+★ 어떻게 준비할까 
+
+
+
+★ 무슨 일을 할까
+
+
+
+
+
