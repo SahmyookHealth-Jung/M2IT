9a4b8d93ee2528c8fc1ff976b2356dec606817dd
정승우 <downersw@gmail.com>
2025-10-21 15:54:58 +0900

    유효성 검사 완료

---
+++ Apps/Temp_Input/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Temp_Input
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTemp = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(100, 122);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(306, 25);
+            this.txtTemp.TabIndex = 0;
+            this.txtTemp.TextChanged += new System.EventHandler(this.CheckTemperatureInput_TextChanged);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Location = new System.Drawing.Point(482, 122);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(276, 23);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "상태확인용";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(326, 212);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "저장확인용";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.txtTemp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTemp;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSave;
+    }
+}
+
---
+++ Apps/Temp_Input/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temp_Input
+{
+    public partial class Form1 : Form
+    {
+        // 체온의 정상 범위 상수
+        private const decimal MIN_NORMAL = 35.0M;
+        private const decimal MAX_NORMAL = 40.0M;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 실시간으로 체온 입력의 유효성을 검사하고 사용자에게 피드백 제공
+        /// 숫자 형식, 소숫점 자릿수, 정상/위험 범위
+        /// </summary>
+        private void CheckTemperatureInput_TextChanged(object sender, EventArgs e)
+        {
+            decimal temp;
+            string input = txtTemp.Text;
+            //bool isTemperatureCorrect = decimal.TryParse(txtTemp.Text, out temp);
+
+            // 숫자인지 유효성 검사
+            if (!decimal.TryParse(input, out temp))
+            {
+                lblStatus.Text = "숫자 형식으로 입력해주세요";
+                lblStatus.ForeColor = Color.Red;
+                return;
+            }
+
+            // 소수점 검사
+            if (input.Contains("."))
+            {
+                int decimalIndex = input.IndexOf(".");
+                // 소수점 둘째 짜리 이상이면
+                if (input.Length - 1 - decimalIndex > 1)
+                {
+                    lblStatus.Text = "소수점 첫째 자리까지만 입력해주세요.";
+                    lblStatus.ForeColor = Color.Blue;
+                    return;
+                }
+            }
+
+            // 경계 조건 검사 
+            if (temp < MIN_NORMAL)
+            {
+                lblStatus.Text = "저체온입니다";
+                lblStatus.ForeColor = Color.Blue;
+                return;
+            }
+            else if (temp > MAX_NORMAL)
+            {
+                lblStatus.Text = "고체온입니다.";
+                lblStatus.ForeColor = Color.Red;
+                return;
+            }
+            else
+            {
+                lblStatus.Text = "정상입니다";
+                lblStatus.ForeColor = Color.Green;
+                return;
+            }
+        }
+
+        /// <summary>
+        /// 저장 시 사용할 최종 유효성 검사 로직
+        /// 실시간 피드백과 분리되어 순수한 데이터 무결성만 검사 (private bool)
+        /// </summary>
+        private bool IsInputValid(out decimal validTemp)
+        {
+            string input = txtTemp.Text;
+            decimal temp;
+
+            if (!decimal.TryParse(input, out temp))
+            {
+                validTemp = 0.0M;
+                return false;
+            }
+
+            // 소숫점 두자리 검사
+            if (input.Contains("."))
+            {
+                int decimalIndex = input.IndexOf(".");
+
+                if (input.Length - 1 - decimalIndex > 1)
+                {
+                    validTemp = 0.0M;
+                    return false;
+                }
+            }
+
+            if (temp < 10.0M || temp > 50.0M)
+            {
+                validTemp = 0.0M;
+                return false;
+            }
+
+            // 모든 검사 통과
+            validTemp = temp;
+            return true;
+        }
+    
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            decimal finalTemp;
+
+            if (IsInputValid(out finalTemp)) { 
+
+                // 모든 검사를 통과하고 저장 가능한 값인 경우
+                // 실제 병원 시스템에서는 DB에 Insert하는 코드가 들어갑니다.
+                MessageBox.Show($"체온 {finalTemp.ToString("N1")}°C 가 성공적으로 저장되었습니다.", "저장 완료");
+                Console.WriteLine($"[저장된 데이터] 체온: {finalTemp}°C");
+                
+                // UI 초기화
+                txtTemp.Clear();
+                lblStatus.Text = "입력 대기중";
+                lblStatus.ForeColor = Color.Black;
+            }
+            else 
+            {
+                MessageBox.Show("입력된 체온 값이 유효하지 않거나 형식이 올바르지 않습니다. 다시 확인해주세요.", "저장 실패", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        }
+    }
+
