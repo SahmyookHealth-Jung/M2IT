76af868008c3675fc1e652165b21e5e726ce03fd
정승우 <downersw@gmail.com>
2025-10-21 17:45:30 +0900

    DB 연동

---
+++ Apps/DB_Practice/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace DB_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(68, 75);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(384, 25);
+            this.txtId.TabIndex = 0;
+            // 
+            // btnFind
+            // 
+            this.btnFind.Location = new System.Drawing.Point(553, 77);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(157, 23);
+            this.btnFind.TabIndex = 1;
+            this.btnFind.Text = "입력";
+            this.btnFind.UseVisualStyleBackColor = true;
+            this.btnFind.Click += new System.EventHandler(this.btnFind_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.Location = new System.Drawing.Point(68, 141);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(384, 23);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "이름";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.txtId);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ Apps/DB_Practice/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace DB_Practice
+{
+    public partial class Form1 : Form
+    {
+        private string connectingString =
+            "Server=localhost;Port=3306;Database=db_practice;Uid=downer;Pwd=dkdlxl123!";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private string FindUserName(string searchId)
+        {
+            string userName = null;
+
+            using (MySqlConnection conn = new MySqlConnection(connectingString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    string sql = "SELECT UserName From Users WHERE UserId = @id";
+
+                    using (MySqlCommand cmd = new MySqlCommand(sql, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@id", searchId);
+                        using (MySqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                userName = reader.GetString("UserName");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"DB 오류 발생 : {ex.Message}");
+                }
+            }
+            return userName ?? "회원을 찾을 수 없습니다.";
+        }
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            string inputId = txtId.Text.Trim();
+
+            string foundName = FindUserName(inputId);
+            lblName.Text = foundName;
+
+            if(foundName.Contains("찾을 수 없습니다."))
+            {
+                lblName.ForeColor = Color.Red;
+            }
+            else
+            {
+                lblName.ForeColor = Color.Black;
+            }
+        }
+    }
+}
