ce775d47e38d6e23be53e4e778e20b43f50e299b
정승우 <downersw@gmail.com>
2025-10-21 14:03:03 +0900

    BMI 유효성 검사 연습

---
+++ Apps/BMI/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace BMI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBMIValue = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(106, 120);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(143, 25);
+            this.txtHeight.TabIndex = 0;
+            this.txtHeight.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(106, 164);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(143, 25);
+            this.txtWeight.TabIndex = 1;
+            this.txtWeight.TextChanged += new System.EventHandler(this.Input_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "키";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "체중";
+            // 
+            // lblBMIValue
+            // 
+            this.lblBMIValue.Location = new System.Drawing.Point(392, 130);
+            this.lblBMIValue.Name = "lblBMIValue";
+            this.lblBMIValue.Size = new System.Drawing.Size(190, 15);
+            this.lblBMIValue.TabIndex = 4;
+            this.lblBMIValue.Text = "걔산된 BMI 수치 표시";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(392, 174);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(157, 15);
+            this.lblResult.TabIndex = 5;
+            this.lblResult.Text = "비만도 판정 결과 표시";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblBMIValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.txtHeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblBMIValue;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Apps/BMI/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 상태 정의 : 기억해야 할 핵심 데이터 
+        /// decimal bmiResult; 라고만 작성해도 자동으로 0.0M이 할당된다.
+        /// 코드를 읽는 사람에게 이 변수의 시작 값이 무엇인지 알려주기 위한 좋은 관례
+        /// </summary> 
+        private decimal bmiResult = 0.0M;
+        private string classification = "데이터 없음";
+        public Form1()
+        {
+            InitializeComponent();
+
+            UpdateDisplay();
+        }
+        /// <summary>
+        /// 이벤트 핸들러 : 키/체중이 입력이 바뀔 때마다 실행된다. (TextChanged에 연결됨)
+        /// </summary>       
+        private void Input_TextChanged(object sender, EventArgs e)
+        {
+            CalculatorBMI();    // 핵심 계산 함수 호출
+        }
+        // 핵심 계산 함수
+        private void CalculatorBMI()
+        {
+            decimal weight, height;     // 계산이 끝나면 그 값이 필요없기 때문에 지역변수로 선언
+
+            // 유효성 검사
+            bool isHeightValid = decimal.TryParse(txtHeight.Text, out height);
+            bool isWeightValid = decimal.TryParse(txtWeight.Text, out weight);
+
+            if (isHeightValid && isWeightValid)
+            {
+
+                if (height <= 0M && weight <= 0M)
+                {
+                    bmiResult = 0.0M;
+                    classification = "입력 오류";
+                    UpdateDisplay();
+                    return;
+                }
+
+                // 키(CM)를 미터(m)으로 변환 
+                decimal heightInMeters = height / 100M;
+
+                // bmi 계산 
+                bmiResult = weight / (heightInMeters * heightInMeters);
+
+                ClassifyBMI();
+            }
+            else if (!string.IsNullOrEmpty(txtHeight.Text) || !string.IsNullOrEmpty(txtWeight.Text))
+            {
+                bmiResult = 0.0M;
+                classification = "오류";
+            }
+            else
+            {
+                bmiResult = 0.0M;
+                classification = "데이터 없음";
+            }
+
+            UpdateDisplay();
+        }    
+
+        private void ClassifyBMI()
+        {   
+            if (bmiResult < 18.5M)
+            {
+               classification = "저체중";
+            }
+            else if (bmiResult < 23M)
+            {
+                classification = "정상";
+            }
+            else if (bmiResult < 25M)
+            {
+                classification = "과체중";
+            }
+            else
+            {
+                classification = "비만";
+            }
+        }
+        private void UpdateDisplay()
+        {
+            lblBMIValue.Text = bmiResult.ToString("N2");
+            lblResult.Text = classification;
+        }
+    }
+}
