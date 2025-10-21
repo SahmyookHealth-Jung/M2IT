a3854e90d3f3e5ceb728aaaadcd508901f70744f
정승우 <downersw@gmail.com>
2025-10-21 10:21:52 +0900

    To_Do 수정본

---
+++ Apps/TODO/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace TODO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNewTask = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNewTask
+            // 
+            this.txtNewTask.Location = new System.Drawing.Point(65, 94);
+            this.txtNewTask.Name = "txtNewTask";
+            this.txtNewTask.Size = new System.Drawing.Size(415, 25);
+            this.txtNewTask.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(501, 86);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(152, 37);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "추가";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 15;
+            this.lstTasks.Location = new System.Drawing.Point(65, 143);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(588, 124);
+            this.lstTasks.TabIndex = 2;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(65, 284);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(588, 35);
+            this.btnRemove.TabIndex = 3;
+            this.btnRemove.Text = "삭제";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtNewTask);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNewTask;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Button btnRemove;
+    }
+}
+
---
+++ Apps/TODO/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODO
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Item 객체를 저장할 List<Item>을 정의
+        /// </summary>
+        private List<Item> todoList = new List<Item>();
+        public Form1()
+        {
+            InitializeComponent();
+            UpdateTaskListUI();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string task = txtNewTask.Text.Trim();
+
+            if (string.IsNullOrEmpty(task)) return;
+            // 객체 생성
+            Item newItem = new Item(task);
+            // List<Item>에 객체 저장
+            todoList.Add(newItem);
+
+            txtNewTask.Clear();
+            UpdateTaskListUI();
+            
+        }
+        /// <summary>
+        /// 데이터는 List<Item> 변수에 저장이 되어 있기 때문에 초기화를 해도 문제 발생 X
+        /// </summary>
+        private void UpdateTaskListUI()
+        { 
+            lstTasks.Items.Clear();
+            foreach (Item item in todoList)
+            {
+                lstTasks.Items.Add(item);
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            // 선택된 항목이 있는지 확인
+            int selectedIndex = lstTasks.SelectedIndex;
+
+            if (selectedIndex != -1) 
+            {   
+                // todoList에서 해당 인덱스의 Item 객체를 제거
+                todoList.RemoveAt(selectedIndex);
+                UpdateTaskListUI(); 
+            }
+        }
+    }
+
+    public class Item
+    {
+        public string Name { get; set; }
+        public Item(string name)
+        {
+            this.Name = name;
+        }
+        /// <summary>
+        /// ListBox가 이 메서드를 호출하여 화면에 표시할 문자열을 가져옵니다.
+        /// 우리는 Name 속성을 표시하고 싶으므로 Name을 반환하도록 재정의합니다.
+        /// </summary>     
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
