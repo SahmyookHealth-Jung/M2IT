1f357c499c6799c076b4c68b1f37130c148049c5
정승우 <downersw@gmail.com>
2025-10-21 09:24:06 +0900

    Timer 공부

--- Apps/Review_List/Form1.cs
+++ Apps/Review_List/Form1.cs
@@ -17,7 +17,13 @@ namespace Review_List
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// 버튼을 누르면 텍스트 박스에 입력한 값을 task 변수에 저장하고
+        /// listBox.Items.Add(task)를 해서 새 항목으로 추가하며
+        /// 새로운 Item 객체를 생성하여 레이블에 생성 완료 메세지를 표시한 후
+        /// 텍스트 박스의 내용을 지운다.
+        /// </summary>
+       
         private void btnAdd_Click(object sender, EventArgs e)
         {
             string task = txtInput.Text.Trim();
---
+++ Apps/TimeController/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimeController
+{
+    public partial class Form1 : Form
+    {
+        private DateTime currentTime;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 앱이 시작할 때 초기 시간으로 상태를 설정
+            currentTime = DateTime.Now;
+            clockTimer.Interval = 1000;
+            clockTimer.Start();
+
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {          
+            {
+                // 시간 형식 지정 
+                lblDigitalClock.Text = currentTime.ToString("HH:mm:ss");
+            }
+        }
+
+        private void clockTimer_Tick(object sender, EventArgs e)
+        {
+            // 시스템의 현재시간을 가져와 상태 업데이트
+            currentTime = DateTime.Now; 
+
+            UpdateDisplay();
+        }
+    }
+}
